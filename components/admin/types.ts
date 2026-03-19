c4ce29f04fd7b22d4fdde596bc1fe23c57299604
Eduardo Roldan <eduroldanisaac@gmail.com>
2026-03-19 08:06:45 +0100

    Fix dashboard founder gender typing

--- components/admin/types.ts
+++ components/admin/types.ts
@@ -59,7 +59,7 @@ export type StartupMemberRecord = {
 export type StartupMemberWithProfileRecord = StartupMemberRecord & {
   profile: Pick<
     ProfileRecord,
-    "id" | "first_name" | "last_name" | "email" | "linkedin_url" | "website_url"
+    "id" | "first_name" | "last_name" | "gender" | "email" | "linkedin_url" | "website_url"
   > | null;
 };
 
